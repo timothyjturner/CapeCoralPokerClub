0d4fc409448c5f6cda053122cd37197b907f521d
timothyjturner <timothyjturner@gmail.com>
2024-02-22 16:43:09 -0500

    css fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <div className="py-2 flex flex-col lg:flex-row items-center">
+        <div className="py-2 flex flex-col lg:flex-row items-center justify-center">
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <p className="mx-auto px-4">© Cape Coral Poker Club</p>
           </div>
