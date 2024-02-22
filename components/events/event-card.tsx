8bf9032ba32bee9fcf70f00325fa728b935585f3
timothyjturner <timothyjturner@gmail.com>
2024-02-22 17:06:03 -0500

    css fix

--- components/events/event-card.tsx
+++ components/events/event-card.tsx
@@ -3,7 +3,7 @@ import { Box } from '@mui/material';
 
 
 export default function EventCard({children, order}) {
-  let boxClass = "ccpc-event w-max h-30 bg-slate-100 border-1 mb-4 flex"; 
+  let boxClass = "ccpc-event w-max h-30 bg-slate-100 border-1 mb-4 flex items-center"; 
   if(order == "1") {
     boxClass += " ccpc-event__sticky";
   }
